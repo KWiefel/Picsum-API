cfcbb4ad088e59657b8c23aa39a5b5bc81ef3abd
KWiefel <dev.kwiefel427@gmail.com>
2023-11-16 13:47:03 +0100

    Ready

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+fetch("https://picsum.photos/v2/list")
+  .then((response) => {
+    if (response.ok === false) {
+      throw new Error("Hier ist etwas schief gelaufen");
+    }
+    return response.json();
+  })
+  .then((dataArr) => {
+    console.log(dataArr);
+
+    for (let data of dataArr) {
+      let fig = document.createElement("figure");
+      let img = document.createElement("img");
+      let figCap = document.createElement("figcaption");
+      let btn = document.createElement("button");
+      let a = document.createElement("a");
+
+      img.src = data.download_url;
+      figCap.textContent = data.author;
+
+      a.href = data.download_url;
+      a.target = "_blank";
+      a.textContent = "See more";
+
+      btn.append(a);
+      fig.append(img, figCap, btn);
+      document.body.append(fig);
+    }
+  });
